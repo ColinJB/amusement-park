bb3bdd7ea8b9491d822c6d90d02467115163937e
Colin Bloom and Chris Finney <colin@>
2017-02-16 14:53:22 -0800

    add button and highlight rides

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
 $(function() {
+
+
   var height = parseInt(prompt("What is your height? (in inches)"));
 
-  if (height > 90) {
+  // while (height === NaN) {
+  //   prompt("whatup");
+  // }
+
+   if (height > 90) {
     $("#tooTall").show();
   } else if (height >= 68) {
     $("#tallEnough").show();
   } else {
     $("#tooShort").show();
   }
+
+  $(".btn").click(function() {
+      debugger;
+    $("#tooShort, #tooTall, #tallEnough").show();
+    $(this).siblings("ul").addClass("highlight");
+    $(".btn").hide();
+  });
 });
